623a261c00c52b31e29aec8f835104d485947f09
JaeHunRo <jhr24@duke.edu>
2016-11-10 03:40:58 -0500

    Adds basic routing for authentication and postgres db connection. Basic html files for testing purposes to make api/server calls.

--- app/app.js
+++ app/app.js
@@ -1,21 +1,66 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+
+var userController = require('./controllers/userController');
+var topicController = require('./controllers/topicController');
 
 var app = express();
+var router = express.Router();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// session configuration
+app.use(session({ secret: 'cs316projectsession' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// the following are routes
+require('../config/passport')(passport); // pass passport for configuration
 
-app.route('/topic')
-	// POST /topic - creates basic topic thread
+/* 
+ =====================================
+ FACEBOOK ROUTES =====================
+ =====================================
+*/
+
+// route for facebook authentication and login
+router.route('/login')
+	.get(passport.authenticate('facebook'));
+
+// handle the callback after facebook has authenticated the user
+router.route('/login/callback')
+	.get(passport.authenticate('facebook', { failureRedirect: '/' }), 
+		function (req, res, next) {
+			res.redirect('/topic');
+		});
+
+router.route('/logout')
 	.get(function(req, res) {
-		res.send('hello world!');
+		req.logout();
+		res.redirect('/');
 	});
+
+/*
+ =====================================
+ CUSTOM ROUTES =====================
+ =====================================
+*/
+
+router.route('/topic')
+	.get(userController.isLoggedIn, function(req, res) {
+		// code here to direct to actual page?
+		res.sendFile(path.resolve(__dirname + '/../public/topic.html'));
+	})
+	.post(userController.isLoggedIn, topicController.postTopic);
+
+app.use('/', router);
+
+// serve static file
+app.use(express.static(__dirname + '/../public'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 #!/usr/bin/env node
 
 var app = require('./app/app.js');
+var db = require('./app/models/');
 
-// connect postgres
+/*
+ Test postgres connection
+*/
+db.sequelize
+	.authenticate()
+	.then(function(err) {
+	  console.log('Connection has been established successfully.');
+	}, function (err) { 
+	  console.log('Unable to connect to the database:', err);
+	});
 
-/**
- * Get port from environment and store in Express.
- */
+/*
+ Get port from environment and store in Express.
+*/
 
 var port = process.env.PORT || '3001';
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-app.listen(port);
+/*
+ Sync postgres db with sequelize models and listen on provided port, on all network interfaces and connect to db.
+*/
+db.sequelize.sync().then(function() {
+	app.listen(port, function() {
+		console.log("Listening on port: " + port);
+	});
+});
