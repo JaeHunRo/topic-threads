ef9522669fd4eed8dba7b000069c4870fa7763e3
JaeHunRo <jhr24@duke.edu>
2016-12-12 05:20:13 -0500

    Adding auth file. Not the best

---
+++ config/auth.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'FACEBOOK_APP_ID'      : '1799462417008461', // your App ID
+  'FACEBOOK_APP_SECRET'  : '87b58f37918dd4a9e74716a41c6e2e65', // your App Secret
+  'FACEBOOK_CALLBACK'		: 'http://localhost:3001/login/callback'
+};
